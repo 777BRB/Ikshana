f3a78744193ce6471895fda647e2af8ed240bf4f
Bidhan Ranjan Bhoi <124060161+777BRB@users.noreply.github.com>
2025-12-18 16:42:25 +0530

    Replace labels with headings in Upload component

--- frontend_v2/src/pages/Upload.jsx
+++ frontend_v2/src/pages/Upload.jsx
@@ -10,14 +10,14 @@ import {NavLink} from 'react-router-dom'
          
           
           <button style={{padding:"160px", position:"relative", right:"-60px", borderRadius:"10px", outline:"1px solid white", border:"none", top:"40px", width:"500px", height:"300px"}}>
-          <label style={{position:"relative", top:"100px", left:"-50px", fontSize:"16px"}}>Upload image here!</label>
+          <h2 style={{position:"relative", top:"100px", left:"-50px", fontSize:"16px"}}>Upload image here!</h2>
           <img src={img} alt="" style={{width:"100px", position:"relative", left:"40px", bottom:"100px"}} />
           {/* <label htmlFor="name" style={{position:"relative", top:"0px", letterSpacing:"5px", fontSize:"20px", right:"50px",}}>Upload file here!</label> */}
           <NavLink to="/analysis" style={{textDecoration:"none"}}><button style={{position:"relative", top:"-5px", padding:"10px", right:"-40px", borderRadius:"10px", cursor:"pointer", color:"white", background:"black"}}>Click Me!</button></NavLink>
           </button>
 
           <button style={{padding:"160px", position:"relative", right:"-130px", borderRadius:"10px", outline:"1px solid white", border:"none", top:"40px", width:"500px", height:"300px"}}>
-          <label style={{position:"relative", top:"100px", left:"-60px", fontSize:"16px", display:"block"}}>Enter Co-ordinates here!!</label>
+          <h2 style={{position:"relative", top:"100px", left:"-60px", fontSize:"16px", display:"block"}}>Enter Co-ordinates here!!</h2>
           <img src={co} alt="" style={{width:"100px", position:"relative", left:"40px", bottom:"100px"}} />
           {/* <label htmlFor="name" style={{position:"relative", top:"0px", letterSpacing:"5px", fontSize:"20px", right:"50px",}}>Upload file here!</label> */}
           <NavLink to="/Co" style={{textDecoration:"none"}}><button style={{position:"relative", top:"-5px", padding:"10px", right:"-40px", borderRadius:"10px", cursor:"pointer", color:"white", background:"black"}}>Click Me!</button></NavLink>
